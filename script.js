37fcc87bb75aa594b6d1f8027e5ac12081a27ec3
Jenny Peers <jcmansley-peers@hotmail.com>
2022-09-09 13:52:57 +0100

    refactored my JS

--- script.js
+++ script.js
@@ -1,41 +1,64 @@
 const toggleButton = document.getElementById("toggle-button");
 
 toggleButton.addEventListener("click", function () {
+  makeBodyDark();
+  makeTopNavDark();
+  makeNavSubjectDark();
+  makeNavSubjectsDark();
+  makeMainTitleDark();
+  makeCodeImageTextDark();
+  makeCodingLinkJourneyDark();
+  makeSmallLinkDark();
+  makeFooterNameDark();
+});
+
+function makeBodyDark() {
   const body = document.body;
   body.classList.toggle("dark-mode");
+}
 
-  // const header = document.getElementsByTagName("header");
-  // header[0].classList.toggle("dark-mode");
-
+function makeTopNavDark() {
   const topNav = document.getElementsByClassName("topnav");
   topNav[0].classList.toggle("dark-mode");
+}
 
+function makeNavSubjectDark() {
   const navSubject = document.getElementsByClassName("nav-subject");
   navSubject[0].classList.toggle("dark-mode-text");
+}
 
+function makeNavSubjectsDark() {
   const navSubjects = document.getElementsByClassName("nav-subjects");
-  navSubjects[0].classList.toggle("dark-mode-text");
-  navSubjects[1].classList.toggle("dark-mode-text");
-  navSubjects[2].classList.toggle("dark-mode-text");
-  navSubjects[3].classList.toggle("dark-mode-text");
 
+  for (let i = 0; i < navSubjects.length; i++) {
+    navSubjects[i].classList.toggle("dark-mode-text");
+  }
+}
+function makeMainTitleDark() {
   const mainTitle = document.getElementsByClassName("main-title");
   mainTitle[0].classList.toggle("dark-mode-text");
+}
 
+function makeCodeImageTextDark() {
   const codeImageText = document.getElementsByClassName("code-image-text");
   codeImageText[0].classList.toggle("dark-mode-text");
+}
 
+function makeCodingLinkJourneyDark() {
   const codingJourneyLink = document.getElementsByClassName(
     "coding-journey-link"
   );
   codingJourneyLink[0].classList.toggle("dark-mode-text");
+}
 
+function makeSmallLinkDark() {
   const smallLink = document.getElementsByClassName("small-link");
-  smallLink[0].classList.toggle("dark-mode-text");
-  smallLink[1].classList.toggle("dark-mode-text");
-  smallLink[2].classList.toggle("dark-mode-text");
-  smallLink[3].classList.toggle("dark-mode-text");
+  for (let i = 0; i < smallLink.length; i++) {
+    smallLink[i].classList.toggle("dark-mode-text");
+  }
+}
 
+function makeFooterNameDark() {
   const footerName = document.getElementsByClassName("footer-name");
   footerName[0].classList.toggle("dark-mode-text");
-});
+}
